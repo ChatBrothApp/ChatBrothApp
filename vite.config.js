0a0b2b370b403675d651f044c1c496f1c104edf0
ChatBrothApp <frutopia.ec@gmail.com>
2025-04-26 15:34:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './',
   plugins: [react()],
-  root: './', // raíz del proyecto
-  publicDir: 'public', // directorio público
   build: {
-    outDir: 'dist', // salida de construcción
-  },
+    outDir: 'dist'
+  }
 })
