604fa6ee47f3e92bdaf2ec28bebc997566126acc
ChatBrothApp <frutopia.ec@gmail.com>
2025-04-26 15:19:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-import express from 'express'
-import { createServer as createViteServer } from 'vite'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-async function createServer() {
-  const app = express()
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-  // Usar carpeta dist después de construir
-  app.use(express.static(resolve(__dirname, 'dist')))
-
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor iniciado')
-  })
-}
-
-createServer()
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
